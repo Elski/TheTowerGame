ffd3343a7a0d89e37ef3dbc7ac74dcf57f05d780
kacpran <kacpran@op.pl>
2018-06-17 19:15:52 +0200

    nowy wskaznik hp

--- Assets/scripts/Characters/HeroHP.cs
+++ Assets/scripts/Characters/HeroHP.cs
@@ -1,38 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HeroHP : MonoBehaviour {
-    private float maxHP = Hero.curHp;
-    private float previousHp=Hero.curHp;
-    float change;
-    float left;
+   
+    public Text hp;
     // Use this for initialization
     void Start() {
-        change = 0;
-        maxHP = Hero.curHp;
-        previousHp = Hero.curHp;
-        left =maxHP;
+     
     }
 
     // Update is called once per frame
     void Update() {
+        
+             
+        hp.text = (Hero.curHp*100).ToString("N2");
+        //transform.localScale += new Vector3(change,0,0);
+        //  change = 0;
 
-        if (previousHp != Hero.curHp)
-        {
-            change = -(Hero.curHp / maxHP);
-            previousHp = Hero.curHp;
-
-
-            if (change > 1)
-            {
-                change = left;
-            }
-            left = left - change;
-        }
-      
-
-        transform.localScale += new Vector3(change,0,0);
-        change = 0;
-	}
+    }
 }
+
